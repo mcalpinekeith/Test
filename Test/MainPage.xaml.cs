b100a2c0934b6ff5c3323080245d8545f7ae6a2a
mcalpinekeith <mcalpinekeith@yahoo.com>
2018-08-25 03:41:56 -0500

    Initial check-in of module Test

---
+++ Test/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        List<string> _quotes = new List<string>();
+
+        int _quoteIndex = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _quotes.Add("Together we can change the world, just one random act of kindness at a time.");
+            _quotes.Add("My style is unique and random. But I think it's important that it still makes sense.");
+            _quotes.Add("The past cannot be changed.The future is yet in your power.");
+
+            //switch(Device.RuntimePlatform)
+            //{
+            //    case "iOS":
+            //        Padding = new Thickness(0, 20, 0, 0);
+            //        break;
+            //}
+        }
+
+        private void NextButton_Clicked(object sender, System.EventArgs e)
+        {
+            _quoteIndex++;
+
+            if (_quoteIndex >= _quotes.Count) _quoteIndex = 0;
+
+            QuoteLabel.Text = _quotes[_quoteIndex];
+        }
+
+        private void FontSlider_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            var newStep = Math.Round(e.NewValue / 1.0);
+
+            FontSlider.Value = newStep * 1.0;
+        }
+    }
+}
