fb60bf4d691e4875ca9667b4d35c7d2669c570b1
mcalpinekeith <mcalpinekeith@yahoo.com>
2018-09-19 08:47:30 -0500

    playlistvm

---
+++ Test/Mvvm/ViewModels/PlaylistViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+
+namespace Test.Mvvm.ViewModels
+{
+    public class PlaylistViewModel : BaseViewModel
+    {
+        private bool _isFavorite;
+
+        public string Title { get; set; }
+
+        public bool IsFavorite
+        {
+            get => _isFavorite;
+            set
+            {
+                Set(ref _isFavorite, value);
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return IsFavorite ? Color.Pink : Color.Black;
+            }
+        }
+    }
+}
